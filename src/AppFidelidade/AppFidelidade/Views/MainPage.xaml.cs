af84f04035f37467d136c7584b61087916d37174
guilherme rodrigues souza <guilherme-rsouza@hotmail.com>
2017-06-11 00:43:50 -0300

    adicionado projeto xamarin

---
+++ src/AppFidelidade/AppFidelidade/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace AppFidelidade.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
